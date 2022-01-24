2f383ae45ed6a44d11d3063d3c94c9e54ae1825d
Ridwane <ridwanediallo99@gmail.com>
2022-01-24 10:54:51 +0000

    Select open mobile-menu class in the style.js and clicl event on the menu_icon

--- scripts/style.js
+++ scripts/style.js
@@ -1,1 +1,6 @@
-let navlinks = document.querySelector('')
+let menu_icon = document.querySelector(".open");
+let mobileToggle = document.querySelector(".mobile-menu");
+
+menu_icon.addEventListener("click", function () {
+  mobileToggle.classList.toggle('hidden');
+});
