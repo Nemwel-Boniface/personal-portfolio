d1d99b7289f6f4dc6c40819c7b2b0e296f622ca4
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-01-24 20:15:40 +0300

    Update to project mobile menu

--- scripts/style.js
+++ scripts/style.js
@@ -1,14 +1,24 @@
 const menubtn = document.getElementById('navright');
 const mobilemenu = document.querySelector('.mobile-menu');
 const menu = document.getElementById('menu');
+const logo = document.querySelector('.navleft');
 
 mobilemenu.style.display = 'none';
 menubtn.onclick = function () {
   if (mobilemenu.style.display === 'none') {
     mobilemenu.style.display = 'block';
     menu.src = 'images/close.png';
+    menu.style.position = 'relative';
+    menu.style.top = '30px';
+    menu.style.left = '290px';
+    logo.style.display = 'none';
+    // logo.style.color = 'black';
   } else {
     mobilemenu.style.display = 'none';
     menu.src = 'images/menu.png';
+    logo.style.display = 'block';
+    menu.style.position = 'relative';
+    menu.style.top = '0';
+    menu.style.left = '0';
   }
 };
