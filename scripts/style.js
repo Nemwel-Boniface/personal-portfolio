b31ddbf3f43e5f2c5643031b799e579d84e4c8c4
Ridwane <ridwanediallo99@gmail.com>
2022-01-24 12:42:17 +0000

    Update script file

--- scripts/style.js
+++ scripts/style.js
@@ -1,6 +1,13 @@
 let menu_icon = document.querySelector(".open");
 let mobileToggle = document.querySelector(".mobile-menu");
+let close = document.querySelector('.close');
 
 menu_icon.addEventListener("click", function () {
   mobileToggle.classList.toggle('hidden');
 });
+
+// close.addEventListener("click", function () {
+//   mobileToggle.classList.toggle('hidden');
+// });
+
+
