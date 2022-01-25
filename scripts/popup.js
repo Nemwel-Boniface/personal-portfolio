fc70c1069d31f9c47c49218f1a38e321e6562be7
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-01-25 14:26:06 +0300

    Update to project popup menu scripts

---
+++ scripts/popup.js
@@ -0,0 +1,16 @@
+const multipost = document.getElementById('muiltipostbtn');
+const projectList = [
+  {
+    title : "Multi Post Stories",
+    closebtn : "&times;",
+    // languages : ['Html','Bootstrap','Ruby On Rails'],
+    html : 'html',
+    bootstrap : 'bootstrap',
+    ruby : 'Ruby On Rails',
+    projectImage : '../images/featuredWork.png',
+    projectDesc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam amet animi voluptate doloribus at quaerat, vitae dicta sunt possimus voluptatum, facilis vel? Eligendi, harum ipsum excepturi sint at quasi? Repellendus eveniet suscipit quibusdam inventore architecto minus eligendi adipisci. Harum atque voluptate dolore fugiat pariatur eius ullam architecto porro aspernatur nihil.",
+    projectbtnlive : 'https://nemwel-boniface.github.io/personal-portfolio/',
+    projectbtnsource : 'https://nemwel-boniface.github.io/personal-portfolio/'
+  },
+]
+
