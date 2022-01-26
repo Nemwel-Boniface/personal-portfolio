ca3b84e558f0405db8bb16f0f63bfa289a2cf46d
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-01-26 18:28:53 +0300

    Update checked and fixed linter errors

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,5 +1,5 @@
 const body = document.querySelector('body');
-const featuredModal = document.querySelector('.featuredmodal');
+// const featuredModal = document.querySelector('.featuredmodal');
 const featuredbtn = document.getElementById('muiltipostbtn');
 const projectBtn = document.querySelectorAll('.openProject button');
 
@@ -8,40 +8,35 @@ popUp.classList.add('popup');
 
 const projectList = [
   {
-    id : 'proj1',
-    title : "Multi Post Stories",
-    closebtn : `<i class="fa fa-times" aria-hidden="true"></i>`,
-    html : 'html',
-    bootstrap : 'bootstrap',
-    ruby : 'Ruby On Rails',
-    projectImage : './images/mobilemodal.png',
-    projectDesc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam amet animi voluptate doloribus at quaerat, vitae dicta sunt possimus voluptatum, facilis vel? Eligendi, harum ipsum excepturi sint at quasi? Repellendus eveniet suscipit quibusdam inventore architecto minus eligendi adipisci. Harum atque voluptate dolore fugiat pariatur eius ullam architecto porro aspernatur nihil.",
-    projectbtnlive : 'https://nemwel-boniface.github.io/personal-portfolio/',
-    projectbtnsource : 'https://nemwel-boniface.github.io/personal-portfolio/'
+    id: 'proj1',
+    title: 'Multi Post Stories',
+    closebtn: '<i class="fa fa-times" aria-hidden="true"></i>',
+    html: 'html',
+    bootstrap: 'bootstrap',
+    ruby: 'Ruby On Rails',
+    projectImage: './images/mobilemodal.png',
+    projectDesc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam amet animi voluptate doloribus at quaerat, vitae dicta sunt possimus voluptatum, facilis vel? Eligendi, harum ipsum excepturi sint at quasi? Repellendus eveniet suscipit quibusdam inventore architecto minus eligendi adipisci. Harum atque voluptate dolore fugiat pariatur eius ullam architecto porro aspernatur nihil.',
+    projectbtnlive: 'https://nemwel-boniface.github.io/personal-portfolio/',
+    projectbtnsource: 'https://nemwel-boniface.github.io/personal-portfolio/',
   },
   {
-    id : 'proj6',
-    title : "Multi Post Stories",
-    closebtn : `<i class="fa fa-times" aria-hidden="true"></i>`,
-    html : 'html',
-    bootstrap : 'bootstrap',
-    ruby : 'Ruby On Rails',
-    projectImage : './images/mobilemodal.png',
-    projectDesc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam amet animi voluptate doloribus at quaerat, vitae dicta sunt possimus voluptatum, facilis vel? Eligendi, harum ipsum excepturi sint at quasi? Repellendus eveniet suscipit quibusdam inventore architecto minus eligendi adipisci. Harum atque voluptate dolore fugiat pariatur eius ullam architecto porro aspernatur nihil.",
-    projectbtnlive : 'https://nemwel-boniface.github.io/personal-portfolio/',
-    projectbtnsource : 'https://nemwel-boniface.github.io/personal-portfolio/'
-  }
+    id: 'proj6',
+    title: 'Multi Post Stories',
+    closebtn: '<i class="fa fa-times" aria-hidden="true"></i>',
+    html: 'html',
+    bootstrap: 'bootstrap',
+    ruby: 'Ruby On Rails',
+    projectImage: './images/mobilemodal.png',
+    projectDesc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam amet animi voluptate doloribus at quaerat, vitae dicta sunt possimus voluptatum, facilis vel? Eligendi, harum ipsum excepturi sint at quasi? Repellendus eveniet suscipit quibusdam inventore architecto minus eligendi adipisci. Harum atque voluptate dolore fugiat pariatur eius ullam architecto porro aspernatur nihil.',
+    projectbtnlive: 'https://nemwel-boniface.github.io/personal-portfolio/',
+    projectbtnsource: 'https://nemwel-boniface.github.io/personal-portfolio/',
+  },
 ];
 
-
-projectBtn.forEach((project) => {
-  project.addEventListener('click', modalpopup);
-});
-featuredbtn.onclick = modalpopup;
 function modalpopup() {
   popUp.style.display = 'block';
   body.appendChild(popUp);
-  let showing = `
+  const showing = `
   <div class="popupcont">
       <div class="popupcontheader">
         <h2 class="desktophide">${projectList[1].title}</h2>
@@ -73,9 +68,15 @@ function modalpopup() {
   popUp.innerHTML = showing;
   body.appendChild(popUp);
 
-  document.addEventListener('click', (action) => {
-    if (action.target.id === 'closeModal') {
+  document.addEventListener('click', (click) => {
+    if (click.target.id === 'closeModal') {
       popUp.style.display = 'none';
     }
   });
 }
+
+projectBtn.forEach((project) => {
+  project.addEventListener('click', modalpopup);
+});
+
+featuredbtn.onclick = modalpopup;
