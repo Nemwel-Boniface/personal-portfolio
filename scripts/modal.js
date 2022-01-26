e79741a70c05e6bae3622016c77d458f58e7dce7
Ridwane <ridwanediallo99@gmail.com>
2022-01-26 13:24:20 +0000

    Add html code in modal.js

--- scripts/modal.js
+++ scripts/modal.js
@@ -33,3 +33,43 @@ const projectList = [
   }
 ];
 
+
+projectBtn.forEach((project) => {
+  project.addEventListener('click', modalpopup);
+});
+featuredbtn.onclick = modalpopup;
+function modalpopup() {
+  popUp.style.display = 'block';
+  body.appendChild(popUp);
+  let showing = `
+  <div class="popupcont">
+      <div class="popupcontheader">
+        <h2 class="desktophide">${projectList[1].title}</h2>
+        <h2 class="mobilehide">Keeping track of hundreds  of components website</h2>
+        <button id="closeModal" class="closeModal">${projectList[1].closebtn}</button>
+      </div>
+      <div class="popupcontlangs">
+        <ul>
+          <li>${projectList[1].html}</li>
+          <li>${projectList[1].bootstrap}</li>
+          <li>${projectList[1].ruby}</li>
+        </ul>
+      </div>
+      <div class="popupcontbody">
+        <div class="popupcontbodyleft">
+          <img src="${projectList[1].projectImage}" alt="image">
+        </div>
+        <div class="popupcontbodyright">
+          <p class="desktophide">${projectList[1].projectDesc}</p>
+          <p class="mobilehide">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.</p>
+          <div class="popupcontbodyrightbtns">
+          <a href="https://nemwel-boniface.github.io/personal-portfolio/"><button type="button" id="modalbtns live">See Live <img src="./images/viewlive.png" alt="view live"></button></a>
+          <a href="https://github.com/Nemwel-Boniface/personal-portfolio"><button type="button" id="modalbtns live">See Live <img src="./images/viewsource.png" alt="view live"></button></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+  popUp.innerHTML = showing;
+  body.appendChild(popUp);
+}
