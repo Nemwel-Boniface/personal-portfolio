e27928f042f10e0a614e40b7d358f1744c2bd6b1
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-01-27 19:38:54 +0300

    Update to form validation logic

--- scripts/style.js
+++ scripts/style.js
@@ -35,3 +35,17 @@ function hideMenu() {
 lists.forEach((list) => {
   list.addEventListener('click', hideMenu);
 });
+
+
+const form = document.getElementById('form');
+
+function formValidate() {
+  const error = document.getElementById('error');
+  const email = document.getElementById('email');
+
+  if(email.value !== email.value.toLowerCase()) {
+    error.innerHTML = `Your email is not in lowercase. Consider using ${email.value.toLowerCase()} instead`;
+    return false;
+  }
+  return true;
+}
