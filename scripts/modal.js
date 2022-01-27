f3c25c5d0f4f2ba9b41109888bd241c5c7c711d5
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-01-27 14:01:53 +0300

    Update dynamically appending projects section scripts

--- scripts/modal.js
+++ scripts/modal.js
@@ -75,30 +75,29 @@ const projectList = [
   },
 ];
 
-
 function modalpopup() {
   popUp.style.display = 'block';
   body.appendChild(popUp);
   const showing = `
   <div class="popupcont">
       <div class="popupcontheader">
-        <h2 class="desktophide">${projectList[1].title}</h2>
+        <h2 class="desktophide">${projectList[projectList.length - 1].title}</h2>
         <h2 class="mobilehide">Keeping track of hundreds  of components website</h2>
-        <button id="closeModal" class="closeModal">${projectList[1].closebtn}</button>
+        <button id="closeModal" class="closeModal">${projectList[projectList.length - 1].closebtn}</button>
       </div>
       <div class="popupcontlangs">
         <ul>
-          <li>${projectList[1].html}</li>
-          <li>${projectList[1].bootstrap}</li>
-          <li>${projectList[1].ruby}</li>
+          <li>${projectList[projectList.length - 1].html}</li>
+          <li>${projectList[projectList.length - 1].bootstrap}</li>
+          <li>${projectList[projectList.length - 1].ruby}</li>
         </ul>
       </div>
       <div class="popupcontbody">
         <div class="popupcontbodyleft">
-          <img src="${projectList[1].projectImage}" alt="image">
+          <img src="${projectList[projectList.length - 1].projectImage}" alt="image">
         </div>
         <div class="popupcontbodyright">
-          <p class="desktophide">${projectList[1].projectDesc}</p>
+          <p class="desktophide">${projectList[projectList.length - 1].projectDesc}</p>
           <p class="mobilehide">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.</p>
           <div class="popupcontbodyrightbtns">
           <a href="https://nemwel-boniface.github.io/personal-portfolio/"><button type="button" id="modalbtns live">See Live <img src="./images/viewlive.png" alt="view live"></button></a>
@@ -118,8 +117,53 @@ function modalpopup() {
   });
 }
 
-projectBtn.forEach((project) => {
-  project.addEventListener('click', modalpopup);
+document.addEventListener('click', (click) => {
+  if (click.target.id === 'proj1') {
+    modalpopup();
+  }
 });
-
 featuredbtn.onclick = modalpopup;
+
+const projectsSection = document.querySelector('.myProjects');
+ function loopdiv() {
+  projectsSection.innerHTML = '';
+   for(let i = 0; i < projectList.length -1 ; i++) {
+  projectsSection.innerHTML = `${projectsSection.innerHTML}
+  <div class="project">
+           <div class="projectHeader">
+             <h3>${projectList[i].projectHeader}</h3>
+           </div>
+
+           <div class="projectBody">
+             <p>
+             ${projectList[i].projectDesc}
+             </p>
+             <ul>
+               <li class="projectHTML">
+                 <ul>
+                  <li>${projectList[i].projectHtml}</li>
+                 </ul>
+               </li>
+               <li class="projectBootstrap">
+                 <ul>
+                   <li>${projectList[i].projectbootstrap}</li>
+                 </ul>
+               </li>
+               <li class="projectRuby">
+                 <ul>
+                   <li>${projectList[i].projectRuby}</li>
+                 </ul>
+               </li>
+             </ul>
+           </div>
+           <div class="projectButton openProject">
+             <button id="proj1" class="testbtnn" onClick="loopdiv()">${projectList[i].projectButton}</button>
+           </div>
+         </div>`;
+    }
+  }
+
+  window.addEventListener('load', () => {
+    loopdiv();
+   });
+
