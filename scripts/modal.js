b9028fcdbeb7406a5906c0521391065150dff638
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-02-23 07:39:39 +0300

    Update include markup for featured project

--- scripts/modal.js
+++ scripts/modal.js
@@ -4,7 +4,7 @@ const featuredbtn = document.getElementById('muiltipostbtn');
 const popUp = document.createElement('div');
 popUp.classList.add('popup');
 
-const projectList = [
+const myFeaturedProject = [
   {
     id: 'proj1',
     projectHeader: 'Profesional Art Printing Data',
@@ -94,23 +94,23 @@ function modalpopup() {
   const showing = `
   <div class="popupcont">
       <div class="popupcontheader">
-        <h2 class="desktophide">${projectList[projectList.length - 1].title}</h2>
+        <h2 class="desktophide">${myFeaturedProject[0].title}</h2>
         <h2 class="mobilehide">Keeping track of hundreds  of components website</h2>
-        <button id="closeModal" class="closeModal">${projectList[projectList.length - 1].closebtn}</button>
+        <button id="closeModal" class="closeModal">${myFeaturedProject[0].closebtn}</button>
       </div>
       <div class="popupcontlangs">
         <ul>
-          <li>${projectList[projectList.length - 1].html}</li>
-          <li>${projectList[projectList.length - 1].bootstrap}</li>
-          <li>${projectList[projectList.length - 1].ruby}</li>
+          <li>${myFeaturedProject[0].html}</li>
+          <li>${myFeaturedProject[0].bootstrap}</li>
+          <li>${myFeaturedProject[0].ruby}</li>
         </ul>
       </div>
       <div class="popupcontbody">
         <div class="popupcontbodyleft">
-          <img src="${projectList[projectList.length - 1].projectImage}" alt="image">
+          <img src="${myFeaturedProject[0].projectImage}" alt="image">
         </div>
         <div class="popupcontbodyright">
-          <p class="desktophide">${projectList[projectList.length - 1].projectDesc}</p>
+          <p class="desktophide">${myFeaturedProject[0].projectDesc}</p>
           <p class="mobilehide">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.</p>
           <div class="popupcontbodyrightbtns">
           <a href="https://nemwel-boniface.github.io/personal-portfolio/"><button type="button" id="modalbtns live">See Live <img src="./images/viewlive.png" alt="view live"></button></a>
@@ -135,41 +135,85 @@ document.addEventListener('click', (click) => {
     modalpopup();
   }
 });
-featuredbtn.onclick = modalpopup;
+
+function featuredmodalpopup() {
+  popUp.style.display = 'block';
+  body.appendChild(popUp);
+  const showing = `
+  <div class="popupcont">
+      <div class="popupcontheader">
+        <h2 class="desktophide">${myFeaturedProject[0].title}</h2>
+        <h2 class="mobilehide">Keeping track of hundreds  of components website</h2>
+        <button id="closeModal" class="closeModal">${myFeaturedProject[0].closebtn}</button>
+      </div>
+      <div class="popupcontlangs">
+        <ul>
+          <li>${myFeaturedProject[0].html}</li>
+          <li>${myFeaturedProject[0].css}</li>
+          <li>${myFeaturedProject[0].JavaScript}</li>
+        </ul>
+      </div>
+      <div class="popupcontbody">
+        <div class="popupcontbodyleft">
+          <img src="${myFeaturedProject[0].projectImage}" alt="image">
+        </div>
+        <div class="popupcontbodyright">
+          <p class="desktophide">${myFeaturedProject[0].projectDesc}</p>
+          <p class="mobilehide">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.</p>
+          <div class="popupcontbodyrightbtns">
+          <a href="https://nemwel-boniface.github.io/personal-portfolio/"><button type="button" id="modalbtns live">See Live <img src="./images/viewlive.png" alt="view live"></button></a>
+          <a href="https://github.com/Nemwel-Boniface/personal-portfolio"><button type="button" id="modalbtns live">See Source <img src="./images/viewsource.png" alt="view Source"></button></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+  popUp.innerHTML = showing;
+  body.appendChild(popUp);
+
+  document.addEventListener('click', (click) => {
+    if (click.target.id === 'closeModal') {
+      popUp.style.display = 'none';
+    }
+  });
+}
+
+
+featuredbtn.onclick = featuredmodalpopup;
 
 const projectsSection = document.querySelector('.myProjects');
 function loopdiv() {
   projectsSection.innerHTML = '';
-  for (let i = 0; i < projectList.length - 1; i += 1) {
+  for (let i = 0; i < 0; i += 1) {
     projectsSection.innerHTML = `${projectsSection.innerHTML}
   <div class="project">
            <div class="projectHeader">
-             <h3>${projectList[i].projectHeader}</h3>
+             <h3>${myFeaturedProject[i].projectHeader}</h3>
            </div>
            <div class="projectBody">
              <p>
-             ${projectList[i].projectDesc}
+             ${myFeaturedProject[i].projectDesc}
              </p>
              <ul>
                <li class="projectHTML">
                  <ul>
-                  <li>${projectList[i].projectHtml}</li>
+                  <li>${myFeaturedProject[i].projectHtml}</li>
                  </ul>
                </li>
                <li class="projectBootstrap">
                  <ul>
-                   <li>${projectList[i].projectbootstrap}</li>
+                   <li>${myFeaturedProject[i].projectbootstrap}</li>
                  </ul>
                </li>
                <li class="projectRuby">
                  <ul>
-                   <li>${projectList[i].projectRuby}</li>
+                   <li>${myFeaturedProject[i].projectRuby}</li>
                  </ul>
                </li>
              </ul>
            </div>
            <div class="projectButton openProject">
-             <button id="proj1" class="testbtnn" onClick="loopdiv()">${projectList[i].projectButton}</button>
+             <button id="proj1" class="testbtnn" onClick="loopdiv()">${myFeaturedProject[i].projectButton}</button>
            </div>
       </div>`;
   }
